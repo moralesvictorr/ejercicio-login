272b5975bed2c8af18bbc56dddd92eaaeb50b571
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-02 12:03:46 -0500

    feat: login

---
+++ src/components/Logout.jsx
@@ -0,0 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+
+
+const Logout = () => {
+    const { logout } = useAuth0()
+    return <button onClick={() => logout({returnTo:window.location.origin})}>Logout</button>
+}
+
+export default Logout
